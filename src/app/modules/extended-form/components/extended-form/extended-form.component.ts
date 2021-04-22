d319a47c708981c7b3b4f15388fc304c459436af
Damian Pietrzyk <dpietrzyk96@gmail.com>
2021-04-22 10:50:32 +0200

    2

--- src/app/modules/extended-form/components/extended-form/extended-form.component.ts
+++ src/app/modules/extended-form/components/extended-form/extended-form.component.ts
@@ -1,27 +1,62 @@
-import { Component } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-extended-form',
   templateUrl: './extended-form.component.html',
   styleUrls: ['./extended-form.component.scss'],
 })
+export class ExtendedFormComponent implements OnInit {
+  private static readonly LS_FORM_KEY = 'saved-form';
 
-export class ExtendedFormComponent {
   details = '';
 
   form: FormGroup = new FormGroup({
     userData: new FormGroup({
-      name: new FormControl(''),
+      name: new FormControl('', Validators.required),
+      email: new FormControl('',
+        [Validators.required, Validators.email],
+      ),
     }),
+    phone: new FormControl('', Validators.pattern(/\d{3}-\d{3}-\d{3}$/)),
+    country: new FormControl('pol'),
+    newsletter: new FormControl(false),
+    sex: new FormControl('female'),
+    description: new FormControl(''),
   });
 
+  ngOnInit(): void {
+    this.restoreDataFromStorage();
+    this.saveDataToStorageOnFormChange();
+  }
+
   onSubmit(): void {
-    console.log(this.form);
+    console.log(this.form.value);
   }
 
   clearForm(): void {
-    console.log('clearForm');
     this.form.reset();
+  }
+
+  private restoreDataFromStorage(): void {
+    const savedDataRaw = localStorage.getItem(ExtendedFormComponent.LS_FORM_KEY);
+    if (!savedDataRaw) {
+      return;
+    }
+
+    const savedData = JSON.parse(savedDataRaw);
+    this.form.patchValue(savedData);
+    this.form.markAllAsTouched();
+  }
+
+  private saveDataToStorageOnFormChange(): void {
+    this.form.valueChanges.subscribe(val => {
+      if (this.form.pristine) {
+        return;
+      }
+
+      localStorage.setItem(ExtendedFormComponent.LS_FORM_KEY, JSON.stringify(val));
+      console.log(val);
+    });
   }
 }
